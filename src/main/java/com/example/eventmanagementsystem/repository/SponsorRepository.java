080bb0d3b2ee20dcd7e8a97e260d06bcd3e53486
swathiraj <swathiraj687@gmail.com>
2024-03-15 07:30:53 +0000

    solution

--- src/main/java/com/example/eventmanagementsystem/repository/SponsorRepository.java
+++ src/main/java/com/example/eventmanagementsystem/repository/SponsorRepository.java
@@ -19,13 +19,15 @@ public interface SponsorRepository {
 
     ArrayList<Sponsor> getSponsors();
 
-    Sponsor getSponsorById(int sponsorId);
-
-    Sponsor addSponsor(Sponsor sponsor);
-
-    Sponsor updateSponsor(int sponsorId, Sponsor sponsor);
-
-    void deleteSponsor(int sponsorId);
-
-    List<Event> getSponsorEvents(int sponsorId);
+    /*
+     * Sponsor getSponsorById(int sponsorId);
+     * 
+     * Sponsor addSponsor(Sponsor sponsor);
+     * 
+     * Sponsor updateSponsor(int sponsorId, Sponsor sponsor);
+     * 
+     * void deleteSponsor(int sponsorId);
+     * 
+     * List<Event> getSponsorEvents(int sponsorId);
+     */
 }
